cc700df21403cb944979fef318fa9ad841a06dc7
Ekrick <erik.lund@edu.futuregames.nu>
2023-12-01 16:12:14 +0100

    Grid Movement

---
+++ PacmanClone/source/Movement/GridMovement.cpp
@@ -0,0 +1,25 @@
+﻿#include "PacmanCore.h"
+#include "GridMovement.h"
+#include "Grid/GridCell.h"
+#include "Grid/GridLink.h"
+
+std::shared_ptr<GridCell> GridMovement::NextCell(std::shared_ptr<GridCell> currentCell)
+{
+    std::shared_ptr<GridCell> target = currentCell->Links[newDirection]->Target;
+    if (target != nullptr && target->bIsWalkable)
+    {
+        currentDirection = newDirection;
+        return target; 
+    }
+    target = currentCell->Links[currentDirection]->Target;
+    if (target != nullptr && target->bIsWalkable)
+    {
+        return target; 
+    }
+    return currentCell;
+}
+
+void GridMovement::SetDirection(Directions direction)
+{
+    newDirection = direction;
+}
---
+++ PacmanClone/source/Movement/GridMovement.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "PacmanCore.h"
+
+class GridCell;
+
+class GridMovement
+{
+    
+public:
+    std::shared_ptr<GridCell> NextCell(std::shared_ptr<GridCell> currentCell);
+    void SetDirection(Directions direction);
+
+protected:
+    Directions currentDirection {};
+    Directions newDirection {};
+};
