1f82744d217e858c7c3b3b376c05b676d41afe8a
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2023-12-01 12:37:16 +0100

    Basic grid logic

--- PacmanClone/source/Grid/GameGrid.cpp
+++ PacmanClone/source/Grid/GameGrid.cpp
@@ -1,2 +1,37 @@
 #include "PacmanCore.h"
 #include "GameGrid.h"
+
+GameGrid::GameGrid(uint16_t Width, uint16_t Height)
+{
+	this->Width = Width;
+	this->Height = Height;
+
+	Cells.resize(Width * Height);
+	for (int i = 0; i < Width * Height; i++)
+	{
+		Cells[i] = std::make_shared<GridCell>();
+	}
+}
+
+std::shared_ptr<GridCell> GameGrid::GetCellAt(const Point2& Coordinate) const
+{
+	if (Coordinate.x < 0 || Coordinate.y < 0 || Coordinate.x >= Width || Coordinate.y >= Height)
+		return nullptr;
+	uint16_t Index = Coordinate.x * Width + Coordinate.y;
+	return Cells[Index];
+}
+
+Point2 GameGrid::GetCoordinateOf(const std::shared_ptr<GridCell> Cell) const
+{
+	for (int i = 0; i < Width * Height; i++)
+	{
+		if (Cell == Cells[i])
+		{
+			Point2 P;
+			P.x = i / Width;
+			P.y = i % Height;
+			return P;
+		}
+	}
+	return Point2();
+}
--- PacmanClone/source/Grid/GridCell.h
+++ PacmanClone/source/Grid/GridCell.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "PacmanCore.h"
+#include "GridLink.h"
 
 class GridCell
 {
+
+public:
+	GridCell();
+	
 	bool bIsWalkable;
+
+	//Use Directions from Point2.hpp to keep track of indices
+	std::vector<std::shared_ptr<GridLink>> Links;
+
+	
 };
 
