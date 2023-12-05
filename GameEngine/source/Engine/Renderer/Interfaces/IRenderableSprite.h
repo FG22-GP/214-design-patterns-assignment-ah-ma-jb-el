163a0d6e446d09e73cc9f0c2ca76978848d119c7
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2023-12-05 13:34:05 +0100

    IRenderableSprite interface

---
+++ GameEngine/source/Engine/Renderer/Interfaces/IRenderableSprite.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Core.hpp"
+#include <SDL.h>
+
+namespace GameEngine
+{
+	class IRenderableSprite
+	{
+		virtual SDL_Texture* GetTexture() = 0;
+		virtual SDL_Rect* GetCrop() = 0;
+		virtual SDL_Rect* GetRect() = 0;
+		virtual double GetRotation() = 0;
+		virtual SDL_Point* GetCentre() = 0;
+		virtual SDL_RendererFlip GetFlip() = 0;
+	};
+}
+
+
