42b9ab4cd3468b1a65bd3b4358f64fa0c0d6335b
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-12-06 11:45:37 +0100

    Remove incomplete line -> Woops

--- GameEngine/source/Engine/File/FileReader.hpp
+++ GameEngine/source/Engine/File/FileReader.hpp
@@ -7,8 +7,6 @@ class FileReader
 public:
     
     GAME_API static std::vector<std::string> ReadCSVFile(const std::string& filePath);
-
-    GAME_API static
     
 private:
     static bool FileExists(const std::string& filePath)
