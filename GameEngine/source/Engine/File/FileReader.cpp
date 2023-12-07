41ebe7f03023c3cd957487eb564fae55784f156b
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-12-07 14:11:13 +0100

    Better csv reading and string Sanitizer added

--- GameEngine/source/Engine/File/FileReader.cpp
+++ GameEngine/source/Engine/File/FileReader.cpp
@@ -1,37 +1,46 @@
 #include "Core.hpp"
 #include "FileReader.hpp"
 
-std::vector<std::string> FileReader::ReadCSVFile(const std::string& filePath)
+
+// Filepath is relative to the working directory
+
+std::vector<std::string> FileReader::ReadCSVFile(const std::string& filePath, bool bSanitize /*= true*/)
 {
     try
     {
-        if(!FileExists(filePath))
+        if (!FileExists(filePath))
         {
-            throw std::runtime_error("Error : Input file not found or not accessible");
+            throw std::runtime_error("Error: Input file not found or not accessible");
         }
-    
-        std::ifstream file; // Open the filestream
-        file.open(filePath); // Open the file
-        
-        if(!file.is_open()) { // If the file failed to open
-            throw std::runtime_error("Error : Failed to open file");
+
+        std::ifstream file(filePath); // Open the filestream directly with the file path
+
+        if (!file.is_open()) { // If the file failed to open
+            throw std::runtime_error("Error: Failed to open file");
         }
-        
-        std::string line; // String to hold each line
-        std::vector<std::string> lines; // Vector to hold all lines and return
-        while (std::getline(file, line)) {
-            std::stringstream lineStream(line);
-            std::string cell;
-            while (std::getline(lineStream, cell, ',')) {
-                lines.push_back(cell);
-            }
+
+        std::string fileContent((std::istreambuf_iterator(file)), std::istreambuf_iterator<char>());
+        if (bSanitize) {
+            SanitizeString(fileContent); // If sanitization is enabled, remove unwanted characters
         }
+
+        std::stringstream lineStream(fileContent);
+        std::vector<std::string> lines; // Vector to hold all cells and return
+
+        std::string cell;
+        while (std::getline(lineStream, cell, ',')) {
+            lines.push_back(cell); // Splitting cells based on commas and adding to the vector
+        }
+
         file.close(); // Close the filestream
-    
+
         return lines;
-    } catch(const std::exception& e)
+    }
+    catch (const std::exception& e)
     {
         std::cout << e.what() << std::endl;
         return {};
     }
 }
+
+
--- GameEngine/source/Engine/File/FileReader.hpp
+++ GameEngine/source/Engine/File/FileReader.hpp
@@ -6,12 +6,20 @@ class FileReader
 {
 public:
     
-    GAME_API static std::vector<std::string> ReadCSVFile(const std::string& filePath);
+    GAME_API static std::vector<std::string> ReadCSVFile(const std::string& filePath, bool bSanitize = true);
     
 private:
     static bool FileExists(const std::string& filePath)
     {
         const std::ifstream file(filePath);
         return file.good();
+    }
+
+    static void SanitizeString(std::string& input)
+    {
+        std::erase(input, '\n');
+        std::erase(input, '\r');
+        std::erase(input, '\t');
+        std::erase(input, ' ');
     }
 };
