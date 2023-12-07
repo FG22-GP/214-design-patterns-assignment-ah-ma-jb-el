52cc0ee682a04002865ea12d601f06518a043250
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-12-07 12:28:55 +0100

    Hacky input is good to go =D

--- GameEngine/source/Engine/Input/Input.cpp
+++ GameEngine/source/Engine/Input/Input.cpp
@@ -1,46 +1,61 @@
 #include "Core.hpp"
 #include "Input.hpp"
 
-#include <SDL.h>
 #include <SDL_events.h>
 
 namespace GameEngine
 {
     
-    std::unordered_map<SDL_Keycode, std::vector<std::function<void()>>> Input::EventCallbacks;
+    std::unordered_map<KeyboardKey, std::vector<std::function<void()>>> Input::KeyDownCallbacks;
+    std::unordered_map<KeyboardKey, std::vector<std::function<void()>>> Input::KeyUpCallbacks;
 
     void Input::HandleInput()
     {
         SDL_Event Event;
+        std::unordered_map<KeyboardKey, std::vector<std::function<void()>>>* EventCallbacks;
+
         while (SDL_PollEvent(&Event) != 0) {
-            if (Event.type == SDL_KEYDOWN) { // If the event is a key press
-                const SDL_Keycode keyCode = Event.key.keysym.sym; // Get SDL key code from the event
-                // Find corresponding KeyDownEvent in the mapping
-                auto it = EventCallbacks.find(keyCode);
-                if (it != EventCallbacks.end()) {
-                    // Execute callbacks subscribed to this event type
-                    for (auto& callback : it->second) {
-                        callback();
-                    }
+
+            if (Event.type == SDL_KEYDOWN)
+                EventCallbacks = &KeyDownCallbacks;
+            else if (Event.type == SDL_KEYUP)
+                EventCallbacks = &KeyUpCallbacks;
+            else
+                continue;
+            
+            const SDL_Keycode keyCode = Event.key.keysym.sym; // Get SDL key code from the event
+            const auto key = static_cast<KeyboardKey>(keyCode); // Cast SDL key code to KeyboardKey enum
+            
+            // Find corresponding KeyDownEvent in the mapping
+            auto it = EventCallbacks->find(key);
+            if (it != EventCallbacks->end()) {
+                // Execute callbacks subscribed to this event type
+                for (auto& callback : it->second) {
+                    callback();
                 }
             }
+            
         }
     }
 
-    void Input::RegisterInputEvent(const KeyDownEvent keyDownEvent, const std::function<void()>& callback)
+    // A bit hacky, but it works for now
+    void Input::RegisterInputEvent(const KeyEvent keyEvent, const KeyboardKey key, const std::function<void()>& callback)
     {
-        // Get SDL key code from the event
-        const SDL_Keycode keyCode = GetSDLKeyCodeFromEvent(keyDownEvent);
+        std::unordered_map<KeyboardKey, std::vector<std::function<void()>>>& EventCallbacks =
+            (keyEvent == KEY_DOWN_EVENT) ? KeyDownCallbacks : KeyUpCallbacks;
+        
         // Subscribe a callback function to a specific event type
-        EventCallbacks[keyCode].push_back(callback);
+        EventCallbacks[key].push_back(callback);
+        
     }
 
-    void Input::UnRegisterInputEvent(const KeyDownEvent keyDownEvent, const std::function<void()>& callback)
+    void Input::UnRegisterInputEvent(const KeyEvent keyEvent, const KeyboardKey key, const std::function<void()>& callback)
     {
-        // Get SDL key code from the event
-        const SDL_Keycode keyCode = GetSDLKeyCodeFromEvent(keyDownEvent);
+        std::unordered_map<KeyboardKey, std::vector<std::function<void()>>>& EventCallbacks =
+            (keyEvent == KEY_DOWN_EVENT) ? KeyDownCallbacks : KeyUpCallbacks;
+        
         // Unsubscribe a callback function from a specific event type
-        const auto it = EventCallbacks.find(keyCode);
+        const auto it = EventCallbacks.find(key);
         if (it != EventCallbacks.end()) {
             auto& callbacks = it->second;
             // Search for the callback in the subscribed list and remove it if found
---
+++ GameEngine/source/Engine/Input/KeyboardKey.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+// Hacky solution, but it works for now =D
+enum class KeyboardKey
+{
+    None                = 0, // No key pressed
+    W                   = 119,
+    A                   = 97,
+    S                   = 115,
+    D                   = 100,
+    ArrowUp             = 1073741906,
+    ArrowLeft           = 1073741904,
+    ArrowDown           = 1073741905,
+    ArrowRight          = 1073741903,
+    Space               = 32,
+    Enter               = 13,
+    LeftShift           = 1073742049,
+    LeftCtrl            = 1073742048,
+    LeftAlt             = 1073742050,
+    RightShift          = 1073742053,
+    RightCtrl           = 1073742052,
+    RightAlt            = 1073742054,
+    Escape              = 27,
+    F1                  = 1073741882,
+    F2                  = 1073741883,
+    F3                  = 1073741884,
+    F4                  = 1073741885,
+    F5                  = 1073741886,
+    F6                  = 1073741887,
+    F7                  = 1073741888,
+    F8                  = 1073741889,
+    F9                  = 1073741890,
+    F10                 = 1073741891,
+    F11                 = 1073741892,
+    F12                 = 1073741893
+};
