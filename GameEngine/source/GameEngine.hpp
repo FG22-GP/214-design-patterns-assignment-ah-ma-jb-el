2554f660927cc0e9cc59595ef8d5beb4b1f7cad5
MagnuthFG <>
2023-12-08 12:14:46 +0100

    Updated Includes
    Added missing include paths

--- GameEngine/source/GameEngine.hpp
+++ GameEngine/source/GameEngine.hpp
@@ -11,11 +11,19 @@
 
 	#include "Data/Visuals/Texture.hpp"
 	#include "Data/Visuals/Sprite.hpp"
+	#include "Data/Visuals/Interfaces/IAlignedSpriteMap.hpp"
 
 	#include "Data/Transform.hpp"
 
 // ENGINE
 
+	#include "Engine/Asset/AssetLoader.hpp"
+	#include "Engine/File/FileReader.hpp"
+
 	#include "Engine/Application/Application.hpp"
 	#include "Engine/Window/Window.hpp"
 	#include "Engine/Renderer/Renderer.hpp"
+
+	#include "Engine/Input/Input.hpp"
+	#include "Engine/Input/KeyboardKey.h"
+	#include "Engine/Game/GameBase.h"
