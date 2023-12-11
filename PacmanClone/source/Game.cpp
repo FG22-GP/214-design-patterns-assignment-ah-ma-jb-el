e0a9b67cccb5569e79739385e92b1ee74776a2c0
MagnuthFG <>
2023-12-11 10:10:23 +0100

    Improved Project Build
    Disabled console on release build and in engine automated build folder creation before dll export if the game folder doesn't exist yet

--- PacmanClone/source/Game.cpp
+++ PacmanClone/source/Game.cpp
@@ -1,9 +1,11 @@
 #include "PacmanCore.h"
 #include "Game/PacmanGame.h"
 
-// Game Entry
-int main(int argc, char* argv[]){
-	
+#if defined _DEBUG
+int main(int argc, char* argv[]){ // Debug console application
+#else
+int WinMain(int argc, char* argv[]){ // Windows application
+#endif	
 	std::unique_ptr<PacmanGame> Game = std::make_unique<PacmanGame>();
 	Game->Initialize();
 	Game->Run();
