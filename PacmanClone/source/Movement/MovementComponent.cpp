e8651e4821b1b294afbc7ae43593e1ec9413017d
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-12-12 15:41:11 +0100

    First iteration of working move component

--- PacmanClone/source/Movement/MovementComponent.cpp
+++ PacmanClone/source/Movement/MovementComponent.cpp
@@ -2,50 +2,100 @@
 #include "MovementComponent.h"
 #include "World\Actors\Actor.hpp"
 #include "Grid/GridCell.h"
+#include "Grid/GridCellContent.h"
 #include "Grid/GridLink.h"
 #include "Grid/GridWrapLink.h"
 
 void MovementComponent::Tick(float DeltaTime)
 {
     ActorComponent::Tick(DeltaTime);
-    if(currentDirection != cachedDirection)
-        TrySetNewTargetCell();
     Move(DeltaTime);
 }
 
-void MovementComponent::TrySetNewTargetCell()
+bool MovementComponent::TrySetNewTargetCell()
 {
-    const auto NewTarget = GetCurrentCell()->GetLinkInDirection(cachedDirection)->Target;
-    if(NewTarget->bIsPlayerWalkable)
+    if(!CurrentCell->GetLinkInDirection(SteeringDirection))
     {
-        TargetCell = NewTarget;
-        currentDirection = cachedDirection;
+        std::cout << "No link in direction" << std::endl;
+        return false;
     }
+
+    std::shared_ptr<GridCell> NewTarget = CurrentCell->GetLinkInDirection(SteeringDirection)->Target;
+    Directions NewDirection = SteeringDirection;
+    
+    if(!NewTarget->bIsPlayerWalkable)
+    {
+        std::cout << "Target cell: " << NewTarget->Coordinate.ToString() << " is not walkable" << std::endl;
+
+        NewTarget = CurrentCell->GetLinkInDirection(MoveDirection)->Target;
+        NewDirection = MoveDirection;
+        if(!NewTarget->bIsPlayerWalkable)
+        {
+            std::cout << "Target cell: " << NewTarget->Coordinate.ToString() << " is not walkable" << std::endl;
+            return false;
+        }
+    }
+    
+    TargetCell = NewTarget;
+    MoveDirection = NewDirection;
+    std::cout << "New target cell set to: " << TargetCell->Coordinate.ToString() << std::endl;
+
+
+    
+    // targetLink = StartCell->Links[newDirection];
+    // targetCell = targetLink->Target;
+    // if (targetCell && targetCell->bIsWalkable)
+    // {
+    //     bWrapLerp = IsWrapLink(targetLink);
+    //     TargetCell = targetCell;
+    // }
+
+    return true;
 }
 
 void MovementComponent::Move(float DeltaTime)
 {
     if (!TargetCell) { return; }
-    //
-    // const float DistanceToTarget = Vector2::Distance(GetCurrentCell()->Coordinate, TargetCell->Coordinate);
-    // if (DistanceToTarget < 0.01f)
-    // {
-    //     return; // We're already there.
-    // }
-    //
-    // Vector2 MoveVector = GetCurrentCell()->Coordinate - TargetCell->Coordinate;
-    // Vector2::Normalize(MoveVector);
-    //
-    // const float MoveSpeed = 1.0f; // Adjust this speed as needed
-    // const Vector2 DeltaPosition = MoveVector * MoveSpeed * DeltaTime;
-    // const Vector2 NewPosition = GetParent()->ActorTransform.GetLocation() + DeltaPosition;
-    // GetParent()->ActorTransform.SetLocation(NewPosition);
+
+    const std::shared_ptr<Actor> Parent = GetParent();
+    
+    const float DistanceToCurrent = Vector2::Distance(Parent->ActorTransform.GetLocation(), GetCurrentCell()->ActorTransform.GetLocation());
+    const float DistanceToTarget = Vector2::Distance(Parent->ActorTransform.GetLocation(), TargetCell->ActorTransform.GetLocation());
+
+    if(DistanceToTarget < DistanceToCurrent)
+    {
+        OnEnterNewCell(TargetCell);
+    }
+    
+    if (DistanceToTarget < 0.01f)
+    {
+        return; // We're already there.
+    }
+    
+    float MoveX = TargetCell->ActorTransform.GetLocation().X - Parent->ActorTransform.GetLocation().X;
+    float MoveY = TargetCell->ActorTransform.GetLocation().Y - Parent->ActorTransform.GetLocation().Y;
+    
+    MoveX = static_cast<float>(std::clamp(MoveX, -1.0f, 1.0f));
+    MoveY = static_cast<float>(std::clamp(MoveY, -1.0f, 1.0f));
+
+    // Convert to -1, 0, or 1
+    MoveX = static_cast<float>((MoveX > 0) - (MoveX < 0));
+    MoveY = static_cast<float>((MoveY > 0) - (MoveY < 0));
+
+    const Vector2 MoveVector(MoveX, MoveY);
+
+    
+    const float MoveSpeed = 1.0f; // Adjust this speed as needed
+    const Vector2 DeltaPosition = MoveVector * MoveSpeed * DeltaTime;
+    const Vector2 NewPosition = GetParent()->ActorTransform.GetLocation() + DeltaPosition;
+    GetParent()->ActorTransform.SetLocation(NewPosition);
+
 }
 
 Vector2 MovementComponent::WrapLerp(Vector2 start, Vector2 target, float T)
 {
     //placeholder
-    return StartCell->Coordinate;
+    return CurrentCell->Coordinate;
 }
 
 bool MovementComponent::IsWrapLink(std::shared_ptr<GridLink>& link)
@@ -55,18 +105,29 @@ bool MovementComponent::IsWrapLink(std::shared_ptr<GridLink>& link)
     return wraplink != nullptr;
 }
 
+void MovementComponent::OnEnterNewCell(const std::shared_ptr<GridCell>& newCell)
+{
+    if (const std::shared_ptr<GridCellContent> ParentAsGridCellContent = std::dynamic_pointer_cast<GridCellContent>(Parent))
+    {
+        ParentAsGridCellContent->SetCell(newCell);
+    }
+    CurrentCell = newCell;
+    TrySetNewTargetCell();
+
+}
+
 void MovementComponent::SetDirection(Directions newDirection)
 {
-    cachedDirection = newDirection;
-    if(DirectionHelpers::IsOpposingDirection(currentDirection, newDirection)) 
-    {
-        TrySetNewTargetCell();
-    }
+    SteeringDirection = newDirection;
+    TrySetNewTargetCell();
 }
 
 void MovementComponent::Init(std::shared_ptr<GridCell> startCell)
 {
-    StartCell = std::move(startCell);
-    SetDirection(Directions::Right);
-    TargetCell = StartCell->Links[Directions::Right]->Target;
+    CurrentCell = std::move(startCell);
+    SetDirection(Right);
+
+    TargetCell = CurrentCell->Links[SteeringDirection]->Target;
+    TrySetNewTargetCell();
 }
+
--- PacmanClone/source/Movement/MovementComponent.h
+++ PacmanClone/source/Movement/MovementComponent.h
@@ -22,12 +22,12 @@ public:
     void Init(std::shared_ptr<GridCell> startCell);
 
     std::shared_ptr<GridCell> GetTargetCell() { return TargetCell; }
-    std::shared_ptr<GridCell> GetCurrentCell() { return StartCell; }
-    Directions GetCurrentDirection() const { return currentDirection; }
+    std::shared_ptr<GridCell> GetCurrentCell() { return CurrentCell; }
+    Directions GetCurrentDirection() const { return MoveDirection; }
 
 protected:
-    Directions currentDirection = Directions::None;
-    Directions cachedDirection = Directions::None;
+    Directions MoveDirection = Directions::Right;
+    Directions SteeringDirection = Directions::Right;
 
     void Tick(float DeltaTime) override;
 
@@ -35,11 +35,13 @@ private:
     float fTimeElapsed = 0;
     float fLerpDuration = 1;
     bool bWrapLerp = false;
-    std::shared_ptr<GridCell> StartCell = nullptr;
+    std::shared_ptr<GridCell> CurrentCell = nullptr;
     std::shared_ptr<GridCell> TargetCell = nullptr;
 
-    void TrySetNewTargetCell();
+    bool TrySetNewTargetCell();
     void Move(float DeltaTime);
     Vector2 WrapLerp(Vector2 start, Vector2 target, float T);
     bool IsWrapLink(std::shared_ptr<GridLink>& link);
+
+    void OnEnterNewCell(const std::shared_ptr<GridCell>& newCell);
 };
