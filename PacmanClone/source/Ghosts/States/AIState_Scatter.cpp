0cbfbca40c0b571f382363eb5456fd1348dc4d8c
Ekrick <113420224+Ekrick@users.noreply.github.com>
2023-12-14 16:29:40 +0100

    added game over logic to scatter state

--- PacmanClone/source/Ghosts/States/AIState_Scatter.cpp
+++ PacmanClone/source/Ghosts/States/AIState_Scatter.cpp
@@ -40,5 +40,16 @@ void AIState_Scatter::OnStateTick(float DeltaTime)
 
 void AIState_Scatter::OnZakuOverlap()
 {
-    // kill Zaku, game over here
+    std::cout << "Zaku is eaten; you should see You Lose text now" << std::endl;
+    auto World = Ghost->GetWorld();
+    if (World == nullptr) return;
+
+    //Stop game and show lose screen
+    World->bShouldTickActors = false;
+    std::shared_ptr<Actor> WinSplash = World->CreateActor<Actor>();
+    WinSplash->ActorTransform.SetLocation(Vector2(14, 16));
+    WinSplash->ActorTransform.SetScale(Vector2(8, 8));
+    auto spriteComp = WinSplash->AddComponent<SpriteComponent>();
+    auto sprite = AssetLoader::GetSprite("Lost");
+    spriteComp->Initialize(sprite);
 }
